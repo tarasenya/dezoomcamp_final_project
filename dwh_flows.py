d5fa1b482e1883c16e7699c9257b58eb9398ce29
Taras <tarasenya@gmail.com>
2024-03-22 10:28:32 +0100

    Created DWH prefect workflows.

--- dwh_flows.py
+++ dwh_flows.py
@@ -1,32 +1,78 @@
 import pyspark
 from pyspark.sql import SparkSession
 from src.schemas import spark_schema
-from src.definitions import bucket_name
-spark = SparkSession.builder.appName("Optimize BigQuery Storage").config(
+from src.definitions import (
+    bucket_name,
+    project_name,
+    dataset,
+    table_name,
+    HISTORICAL_DATA,
+    HEALTH_DATA,
+)
+from prefect import flow
+
+SPARK_SESSION = (
+    SparkSession.builder.appName("Optimize BigQuery Storage")
+    .config(
         "spark.jars.packages",
         "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.15.1-beta,com.google.cloud.bigdataoss:gcs-connector:hadoop2-2.1.6",
-    ).config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar").getOrCreate()
-
-spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
-spark.conf.set("temporaryGcsBucket",bucket_name)
-
-table = "dataengineeringzoomcamp-409819.week_4_hw.koala"
-
-df = spark.read.format("bigquery").option("table", table).schema(spark_schema).load()
-
-total_koalas_seen = (
-    df.groupBy("sightdate")
-    .sum("numberofkoala")
-    .withColumnRenamed("sum(numberofkoala)", "totalnumberofkoala")
+    )
+    .config(
+        "spark.jars",
+        "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar",
+    )
+    .getOrCreate()
 )
-total_conditions = (
-    df.groupBy("koalacondifinal")
-    .sum("numberofkoala")
-    .withColumnRenamed("sum(numberofkoala)", "totalnumberofkoala")
+
+SPARK_SESSION._jsc.hadoopConfiguration().set(
+    "fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem"
 )
+SPARK_SESSION.conf.set("temporaryGcsBucket", bucket_name)
+
+TABLE_NAME = f"{project_name}.{dataset}.{table_name}"
+
+KOALA_DATA = (
+    SPARK_SESSION.read.format("bigquery")
+    .option("table", TABLE_NAME)
+    .schema(spark_schema)
+    .load()
+)
+
+
+@flow(name="Summarized historical data", log_prints=True)
+def total_number_of_koalas_met():
+    print("Preparing the summarized historical data")
+    historical_data_table = f"{project_name}.{dataset}.{HISTORICAL_DATA}"
+
+    total_koalas_seen = (
+        KOALA_DATA.groupBy("sightdate")
+        .sum("numberofkoala")
+        .withColumnRenamed("sum(numberofkoala)", "totalnumberofkoala")
+    )
+
+    total_koalas_seen.write.format("bigquery").option(
+        "table", historical_data_table
+    ).mode("overwrite").save()
+    print(f"The data has been written to {historical_data_table}.")
+    total_koalas_seen.limit(10).show()
+
+
+@flow(name="Health data", log_prints=True)
+def koala_health_conditions():
+    print("Preparing the summarized health data.")
+    health_data_table = f"{project_name}.{dataset}.{HEALTH_DATA}"
+
+    health_data = (
+        KOALA_DATA.groupBy("koalacondifinal")
+        .sum("numberofkoala")
+        .withColumnRenamed("sum(numberofkoala)", "totalnumberofkoala")
+    )
+    health_data.write.format('bigquery').option('table', health_data_table).mode(
+        'overwrite').save()
+
+    print(f"The data has been written to  {health_data_table}.")
+    health_data.limit(10).show()
+
 
 if __name__ == "__main__":
-    total_conditions.write.format("bigquery").option(
-        "table", "dataengineeringzoomcamp-409819.week_4_hw.koala_health"
-    ).mode('overwrite').save()
-    total_conditions.show()
+    koala_health_conditions()
--- src/definitions.py
+++ src/definitions.py
@@ -7,3 +7,6 @@ project_name = config_parser.get('GCP', 'project')
 dataset = config_parser.get('GCP', 'dataset')
 table_name = config_parser.get('GCP', 'table_name')
 bucket_name = config_parser.get('GCP', 'bucket_name')
+
+HISTORICAL_DATA = 'historical_koala_data'
+HEALTH_DATA = 'health_koala_data'
