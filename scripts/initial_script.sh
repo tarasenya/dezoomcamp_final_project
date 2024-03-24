e583d68a7ae984e18b8db248447239a771c4ceae
Taras <tarasenya@gmail.com>
2024-03-24 18:07:07 +0100

    Changed deployment scripts

--- scripts/initial_script.sh
+++ scripts/initial_script.sh
@@ -2,7 +2,6 @@
 cd /tmp
 git clone https://github.com/tarasenya/dezoomcamp_final_project.git
 cd dezoomcamp_final_project
-pip install -U pip
 pip install pipenv
-pipenv install --system --deploy
+pipenv install --deploy
 pipenv shell
--- scripts/prefect_script.sh
+++ scripts/prefect_script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd ..
-prefect server start &
-sleep 10 && prefect worker start -p general_worker &
-prefect deploy --all --ci && prefect deployment run 'Initial koalas sighting to BQ/initial-ingestion-deployment'
+prefect server start && sleep 10 && prefect worker start -p general_worker && prefect deploy --all --ci && prefect deployment run 'Initial koalas sighting to BQ/initial-ingestion-deployment'
