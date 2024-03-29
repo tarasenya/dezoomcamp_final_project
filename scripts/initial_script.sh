163f2b67082936186c81368bf9f526a19779940d
Taras <tarasenya@gmail.com>
2024-03-29 17:23:16 +0100

    Minro changes of deployment

--- scripts/initial_script.sh
+++ scripts/initial_script.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
-sudo apt-get update 
-sudo apt-get install -yq git python3 python3-pip python3-distutils 
-cd ~
+apt-get update 
+apt-get install -yq git python3 python3-pip python3-distutils 
 git clone -b terraform_deployment https://github.com/tarasenya/dezoomcamp_final_project.git 
 cd dezoomcamp_final_project
 echo 'export PYTHONPATH="${PYTHONPATH}:$(pwd)"' >> ~/.bashrc
-pip3 install pipenv
+pip3 install -U pipenv
 echo 'export "PATH=~/.local/bin:${PATH}"' >> ~/.bashrc
 source ~/.bashrc
-pipenv install --deploy
+pipenv install
 pipenv shell
