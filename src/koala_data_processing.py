26a48b30d20d2431f1a0fe4cf9b305956439e285
Taras <tarasenya@gmail.com>
2024-04-10 12:34:48 +0200

    Linted a code with flake8

--- src/koala_data_processing.py
+++ src/koala_data_processing.py
@@ -1,36 +1,30 @@
+"""
+Processing data from Koala Sighting API.
+"""
 import requests
-from typing import Dict, Optional, List
-from dataclasses import dataclass
-
-
-
-@dataclass
-class KoalaData:
-    objectid: int
-    globalid: str
-    sighttime: int
-    joeypresent: Optional[str]
-    numberofkoala: Optional[int]
-    koalacondifinal: Optional[str]
-    locality: Optional[str]
-    near_koala_habitat: Optional[str]
-    attachment_cc_lic_check: Optional[str]
-    lga: Optional[str]
-    sighting_lat_wgs84: Optional[float]
-    sighting_long_wgs84: Optional[float]
-    vetting_code: Optional[str]
-    gis_x: float
-    gis_y: float
+from typing import Dict, List
+from src.schemas import KoalaData
 
 
 def query_koala_endpoint(where_query: str) -> Dict:
-    address = f"https://spatial-gis.information.qld.gov.au/arcgis/rest/services/QWise/CrocodileSightingsPublicView/FeatureServer/30/query?where={where_query}&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=%7B+%22wkid%22%3A4326+%7D&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&timeReferenceUnknownClient=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson"
+    """
+    Queries the KoalaSighting endpoint using where_query and returns a raw result.
+    :param where_query: defines the date range when a koala has been sighted.
+    :return: data about different koala sightings
+    """
+
+    address = f"https://spatial-gis.information.qld.gov.au/arcgis/rest/services/QWise/CrocodileSightingsPublicView/FeatureServer/30/query?where={where_query}&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=%7B+%22wkid%22%3A4326+%7D&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&timeReferenceUnknownClient=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson"  # noqa E501
     res = requests.get(address)
     print(res.json())
     return res.json()
 
 
 def flatten_koala_response(koala_response: Dict) -> List[KoalaData]:
+    """
+    Preprocessing of a koala sighting response data.
+    :param koala_response: raw response from the koala sighting API.
+    :return: flattening some nested dictionaries.
+    """
     koala_array: List[KoalaData] = []
     for koala_point in koala_response["features"]:
         _properties = koala_point["properties"]
@@ -43,5 +37,3 @@ def flatten_koala_response(koala_response: Dict) -> List[KoalaData]:
             }
         )
     return koala_array
-
-
